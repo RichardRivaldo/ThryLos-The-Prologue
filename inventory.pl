0ac00f169719932d4952698cb9fad003df57a8db
RichardRivaldo <richardrivaldo84@gmail.com>
2020-11-23 11:18:15 +0700

    added more wiki + enemyWiki @maxi

--- inventory.pl
+++ inventory.pl
@@ -64,23 +64,30 @@ wiki :-         write(' __    __ _ _    _   __'), nl,
                 write('                              |___/ '), nl, nl,
 
                 write('WikiLog here, guiding our new players to survive and win the game!'), nl, nl,
-                write('1. In case you have not started your journey yet, use \'start.\' to begin or go to Main Menu!'), nl,
-                write('2. Reveal your identity, starting with small cased letter and end it with \'.\', else our old machine cannot read your name!'), nl,
+                write('0. Navigate to the Main Menu by using the magic words `mainMenu`.'), nl,
+                write('1. In case you have not started your journey yet, use `start.` or go to the Main Menu to begin or go to Main Menu!'), nl,
+                write('2. Reveal your identity, starting with small cased letter and end it with `.`, else our old machine cannot read your name!'), nl,
                 write('3. Choose your class wisely. We keep the secrets of each classes, so you should experiment it all if you want to know more about them!'), nl,
                 write('4. Wait. One more thing about class. Try to find the key to the best class available in the game!'), nl,
                 write('5. After you are registered, you will be given basic starter tools. We want you to survive and not die on the day you start!'), nl,
-                write('6. You can check your stats with the command \'stats.\', never think that you are already strong!'), nl,
-                write('7. These are navigations that you can take from this WikiLog to your desired curiosity: '), nl,
+                write('6. You can check your stats with the command `stats.`, never think that you are already strong!'), nl,
+                write('7. These are navigations that you can take from this WikiLog to your desired curiosity: '), nl, nl,
                 write('   * Enter any other numbers to go back to Main Menu. Basic, isn\'t it?'), nl,
                 write('   * Enter 0 to read the Tales of ThryLos. It is good to know some histories!'), nl,
                 write('   * Enter 1 to know more about your spacial bag. Practical storage is an important thing, you know!'), nl,
                 write('   * Enter 2 to find out more about equipments in our territory. Adapting faster is one way to survive!'), nl,
-                write('   * Enter 3 to have some informations about potions, The Blessings from Gods bestowed upon us!'), nl, nl,
+                write('   * Enter 3 to have some informations about potions, The Blessings from Gods bestowed upon us!'), nl, 
+                write('   * Enter 4 to sneak a peek at places in ThryLos. You should know some geography to some extent too!'), nl,
+                write('   * Enter 5 to get some knowledge on how The Almighty ThryStore works. Be careful on spending your money!'), nl,
+                write('   * Enter 6 to learn about the monsters that invaded ThryLos. It is always good to know about your opponents!'), nl, nl,
                 write('The choice is yours. You do it: '), read_integer(WikiChoice), nl,
                 (WikiChoice = 0 -> about;
                 WikiChoice = 1 -> invWiki;
                 WikiChoice = 2 -> eqWiki;
                 WikiChoice = 3 -> potWiki;
+                WikiChoice = 4 -> mapWiki;
+                WikiChoice = 5 -> storeWiki;
+                WikiChoice = 6 -> enemyWiki;
                 mainMenu).
 
 about :-        write('   _   _                 _   '), nl,
@@ -134,9 +141,7 @@ about :-        write('   _   _                 _   '), nl,
                 write('------------------------------------------------------------------------------------------------------'), nl, nl,
                 write('The word `ThryLos` comes from Greek, meaning `Legend`.'), nl,
                 write('`Vixere` comes from Roman Latin, meaning `They live`, used after the execution of someone.'), nl, nl;
-                write('Curiosity is needed in this world. Even so, we will gladly wait for you to check the Tales of ThryLos!'), nl).
-                
-                
+                write('Curiosity is needed in this world. Even so, we will gladly wait for you to check the Tales of ThryLos!'), nl).          
 
 invWiki :-      write('   ___ '), nl,
                 write('  / __\\ __ _  __ _ '), nl,
@@ -146,10 +151,8 @@ invWiki :-      write('   ___ '), nl,
                 write('             |___/ '), nl, nl,
 
                 write('1. You are given a spacial bag to store your items with maximum space of 100. Only store important items in it!'), nl,
-                write('2. Use the command \'bag.\' to check what you bring with you now. It is always good to be prepared~'), nl,
-                write('3. We do not want you to keep useless items. Use \'throw(item_name, amount).\' to throw it away. DO IT NOW!!!'), nl.
-
-
+                write('2. Use the command `bag.` to check what you bring with you now. It is always good to be prepared~'), nl,
+                write('3. We do not want you to keep useless items. Use `throw(item_name, amount).` to throw it away. DO IT NOW!!!'), nl.
 
 eqWiki :-       write('  _____ _   '), nl,
                 write('  \\_   \\ |_ ___ _ __ ___  ___ '), nl,
@@ -159,19 +162,19 @@ eqWiki :-       write('  _____ _   '), nl,
                 
                 write('1. There are three type of equipments that you can use: Weapon, Armor, and Accessory.'), nl,
                 write('2. Informations are usually classified, but for you, you can check its information with these commands: '), nl,
-                write('   * Weapon Informations    : \'weapon(weapon_name).\''), nl,
-                write('   * Armor Informations     : \'armor(armor_name).\''), nl,
-                write('   * Accessory Informations : \'accessory(accessory_name).\''), nl,
+                write('   * Weapon Informations    : `weapon(weapon_name).`'), nl,
+                write('   * Armor Informations     : `armor(armor_name).`'), nl,
+                write('   * Accessory Informations : `accessory(accessory_name).`'), nl,
                 write('3. Each items are made from different monsters\', or even gods\' blessings. Always use the strongest one that you can equip right away!'), nl,
                 write('4. To ask for the effects of each weapons is very rude! Determine it yourself from your stats if you are using it!'), nl,
                 write('5. To become powerful, suits yourself with your best items. To equip it, you can use the following commands: '), nl,
-                write('   * Equip Weapon    : \'equipWp(weapon_name).\''), nl,
-                write('   * Equip Armor     : \'equipArm(armor_name).\''), nl,
-                write('   * Equip Accessory : \'equipAccs(accessory_name).\''), nl,
+                write('   * Equip Weapon           : `equipWp(weapon_name).`'), nl,
+                write('   * Equip Armor            : `equipArm(armor_name).`'), nl,
+                write('   * Equip Accessory        : `equipAccs(accessory_name).`'), nl,
                 write('6. If you want to unequip your equipments, use these and the item will be stored back to your bag if it still has empty spaces: '), nl,
-                write('   * Unequip Weapon    : \'unequipWp(weapon_name).\''), nl,
-                write('   * Unequip Armor     : \'unequipArm(armor_name).\''), nl,
-                write('   * Unequip Accessory : \'unequipAccs(weapon_name).\''), nl,
+                write('   * Unequip Weapon         : `unequipWp(weapon_name).`'), nl,
+                write('   * Unequip Armor          : `unequipArm(armor_name).`'), nl,
+                write('   * Unequip Accessory      : `unequipAccs(weapon_name).`'), nl,
                 write('7. You can replace your current equipped items without having to unequip the old one first. Easy peasy, bois!'), nl, 
                 write('8. There are five states of items. They give you different buffs for your stats! From lowest to highest: '), nl,
                 write('   * |Ghost States|'), nl,
@@ -180,7 +183,6 @@ eqWiki :-       write('  _____ _   '), nl,
                 write('   * |Lucifer States|'), nl,
                 write('   * |~&^*$%@)~(I*&T|'), nl.
                 
-
 potWiki :-      write('   ___      _   _             '), nl,
                 write('  / _ \\___ | |_(_) ___  _ __  '), nl,
                 write(' / /_)/ _ \\| __| |/ _ \\| \'_ \\ '), nl,
@@ -196,6 +198,73 @@ potWiki :-      write('   ___      _   _             '), nl,
                 write('   * |Defense Potions|'), nl,
                 write('   * |Magic Potions|'), nl,
                 write('   * |Speed Potions|'), nl,
-                write('5. Using the command \'potion(potion_name).\', you can check informations of it. Truly awesome!'), nl,
-                write('6. The only way to respect the Gods is to use the potion by drinking it all. Use \'drink(potion_name).\''), nl.
+                write('5. Using the command `potion(potion_name).`, you can check informations of it. Truly awesome!'), nl,
+                write('6. The only way to respect the Gods is to use the potion by drinking it all. Use `drink(potion_name).`'), nl.
 
+mapWiki :-      write('  /\\/\\   __ _ _ __  ___ '), nl,
+                write(' /    \\ / _` | \'_ \\/ __|'), nl,
+                write('/ /\\/\\ \\ (_| | |_) \\__ \\'), nl,
+                write('\\/    \\/\\__,_| .__/|___/'), nl,
+                write('             |_| '), nl, nl,
+                write('1.  The territory of ThryLos is very diversed, since it was the world of ThryLos affiliated together.'), nl,
+                write('2.  There are 3 immersive regions with different characteristics. They are: '), nl,
+                write('    * Birthright     : The origin that The Legends are born. It is a sacral and starting place for players.'), nl,
+                write('    * Lost Evergreen : The massive forest of ThryLos, now lost and inhabited by the invading monsters. Take it back!'), nl,
+                write('    * DragonLance    : The territory led by The Pinnacle of Dragons, ApeX. To the dungeon and you can kill him there!'), nl,
+                write('3.  Each regions will have different enemies generated. The easiest enemies are of course located in the starting place.'), nl,
+                write('4.  The higher-ups are now focusing on taking back The Lost Evergreen and DragonLance. That\'s why Birthright has very little technologies there!'), nl,
+                write('5.  On the border of each regions, there are boundary walls made by previous Legends in order to prevent monsters from other region to flood in.'), nl,
+                write('6.  You can use the command `map.` to show the map you are currently in. The NVGXor will also display the legends of the map below it.'), nl,
+                write('7.  While you travel across the map, you might encounter some monsters. Kill and loot things from them to get more powerful!'), nl,
+                write('8.  You can\'t go to the coordinates that have walls. Don\'t waste your energy trying it!'), nl,
+                write('9.  You can interact with map objects by going to the coordinate it is placed in. You can reenter by going out and in again.'), nl,
+                write('10. You can teleport within the regions. If the destination is map object, the effects of each object is activated automatically.'), nl,
+                write('11. You can move manually to the next or previous region by going to the `<` or `>` map object. Don\'t stay too long in one region!'), nl,
+                write('12. The magic words that you can use to move across the map: '), nl,
+                write('    * Going Forward  : `w`'), nl,
+                write('    * Turning Left   : `a`'), nl,
+                write('    * Going Backward : `s`'), nl,
+                write('    * Turning Right  : `d`'), nl.
+
+storeWiki :-    write(' __ _                 '), nl,
+                write('/ _\\ |_ ___  _ __ ___ '), nl,
+                write('\\ \\| __/ _ \\| \'__/ _ \\'), nl,
+                write('_\\ \\ || (_) | | |  __/'), nl,
+                write('\\__/\\__\\___/|_|  \\___|'), nl, nl,
+                write('1. The Almighty ThryStore has been there forever, and has been selling high quality potions and equipments.'), nl,
+                write('2. Use the map to know where the ThryStore branch is in the region you are currently in.'), nl,
+                write('3. Navigate to the Store to use to the store. You will only have to enter numbers of your choice to use the store.'), nl,
+                write('4. You can buy potions immediately in the store. Make sure you have the stocks of it or it will be dangerous for you!'), nl,
+                write('5. You can\'t buy equipments. Instead, use the Gacha to get new equipments. Don\'t rage on it if you don\'t get the items you want!'), nl,
+                write('6. There are 5 Gacha systems in the store: '), nl,
+                write('   * Standard : Highest rate on Ghost Grade Equipments.'), nl,
+                write('   * Silver   : Lower rate on Ghost Grade, but higher on other grades.'), nl,
+                write('   * Gold     : Even lower on Ghost, also higher on other grades.'), nl,
+                write('   * Gambler  : Contains Mamon and God Grade only, but 50% chance on getting nothing.'), nl.
+
+enemyWiki   :-  write('   __                     _ '),nl,
+                write('  /__\\ __   ___ _ __ ___ (_) ___  ___ '),nl,
+                write(' /_\\| `_ \\ / _ \\ `_ ` _ \\| |/ _ \\/ __|'),nl,
+                write('//__| | | |  __/ | | | | | |  __/\\__ \\'),nl,
+                write('\\__/|_| |_|\\___|_| |_| |_|_|\\___||___/'),nl,nl,
+
+                write('1. There are five types of enemy monsters that scattered all over the land of ThryLos: Slime, Goblin, Wolf, Spider, and Zool'),nl,
+                write('2. This is the best information that can be gathered from all the people of ThryLos. All just for you, our mighty saviour!'),nl,
+                write('   * |Slime|'),nl,
+                write('     This is the weakest monsters of all out there. Some said that this slimy things are made from Apex\'s sweat.'),nl,
+                write('     A little of advice for you: This type of enemy is very useful to get some exp and gold for early levels!'),nl,
+                write('   * |Goblin|'),nl,
+                write('     Goblin is quite a troublesome enemy because they are well equipped with their selfmade armor,'),nl,
+                write('     their defense is nearly equal with their attack.'),nl,
+                write('   * |Wolf|'),nl,
+                write('     Always be careful when you strolling over this land, because some wolfs are out there lurking for you'),nl,
+                write('     Pay enough attention when you face them, because their speed is high enough before you can even blink!'),nl,
+                write('   * |Spider|'),nl,
+                write('     This scary giant spiders can be very dangerous. You have to look your surrounding! Who knows you can be walking under them.'),nl,
+                write('     Their fangs are the scariest one, be careful not to get bitten!'),nl,
+                write('   * |Zool|'),nl,
+                write('     The champion monster of ThryLos. Their attack and defense is high enough to beat you'),nl,
+                write('     It is really hard to find them but you have to be well prepared when you face them!'),nl,
+                write('3. And finally the last enemy you have to beat to save the land of ThryLos: Apex.'),nl,
+                write('   There is not much information because people are too scared to get close.'),nl,
+                write('4. Remember to always be careful and pay attention to your surrounding because you don\'t know when the monsters will attack you!'),nl.
