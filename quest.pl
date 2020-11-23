014c291212e7cf305eb1342707722ae7cdacf3b7
DaveyJones491 <david.owen.491@gmail.com>
2020-11-23 14:07:42 +0700

    Quest sisa cek lokasi player, update progressnya

--- quest.pl
+++ quest.pl
@@ -29,31 +29,37 @@ generateQuestEasy(level) :- (level = 1 ->
                                 asserta(goblintokill(1)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 2 slime, 1 goblin and 1 wolf'),nl,
+                                generateQuestReward(3,6),
                             level = 2 ->
                                 asserta(slimetokill(1)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 1 slime, 2 goblin and 1 wolf'),nl,
+                                generateQuestReward(5,8),
                             level = 3 ->
                                 asserta(spidertokill(1)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 2 goblin , 1 wolf and 1 spider'),nl,
+                                generateQuestReward(7,10),
                             level = 4 ->
                                 asserta(spidertokill(1)),
                                 asserta(goblintokill(1)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 1 goblin , 2 wolf and 1 spider'),nl,
+                                generateQuestReward(9,12),
                             level = 5 ->
                                 asserta(spidertokill(1)),
                                 asserta(zooltokill(1)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 wolf , 1 spider and 1 zool'),nl,
+                                generateQuestReward(11,14),
                             level = 6 ->
                                 asserta(spidertokill(2)),
                                 asserta(zooltokill(1)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 1 wolf , 2 spider and 1 zool'),nl,
+                                generateQuestReward(13,16),
                             );.
 
 generateQuestMedium(level) :- (level = 1 ->
@@ -61,31 +67,37 @@ generateQuestMedium(level) :- (level = 1 ->
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 2 slime, 2 goblin and 1 wolf'),nl,
+                                generateQuestReward(4,7),
                             level = 2 ->
                                 asserta(slimetokill(1)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 1 slime, 2 goblin and 2 wolf'),nl,
+                                generateQuestReward(6,9),
                             level = 3 ->
                                 asserta(spidertokill(1)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 goblin , 2 wolf and 1 spider'),nl,
+                                generateQuestReward(8,11),
                             level = 4 ->
                                 asserta(spidertokill(2)),
                                 asserta(goblintokill(1)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 1 goblin , 2 wolf and 2 spider'),nl,
+                                generateQuestReward(10,13),
                             level = 5 ->
                                 asserta(spidertokill(2)),
                                 asserta(zooltokill(1)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 wolf , 2 spider and 1 zool'),nl,
+                                generateQuestReward(12,15),
                             level = 6 ->
                                 asserta(spidertokill(2)),
                                 asserta(zooltokill(2)),
                                 asserta(wolftokill(1)),
                                 write('Your Quest is to kill 1 wolf , 2 spider and 2 zool'),nl,
+                                generateQuestReward(14,17),
                             );.
 
 generateQuestHard(level) :- (level = 1 ->
@@ -93,39 +105,52 @@ generateQuestHard(level) :- (level = 1 ->
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 slime, 2 goblin and 2 wolf'),nl,
+                                generateQuestReward(6,9),
                             level = 2 ->
                                 asserta(slimetokill(3)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 3 slime, 2 goblin and 2 wolf'),nl,
+                                generateQuestReward(8,11),
                             level = 3 ->
                                 asserta(spidertokill(2)),
                                 asserta(goblintokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 goblin , 2 wolf and 2 spider'),nl,
+                                generateQuestReward(10,13),
                             level = 4 ->
                                 asserta(spidertokill(2)),
                                 asserta(goblintokill(3)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 3 goblin , 2 wolf and 2 spider'),nl,
+                                generateQuestReward(12,15),
                             level = 5 ->
                                 asserta(spidertokill(2)),
                                 asserta(zooltokill(2)),
                                 asserta(wolftokill(2)),
                                 write('Your Quest is to kill 2 wolf , 2 spider and 2 zool'),nl,
+                                generateQuestReward(14,17),
                             level = 6 ->
                                 asserta(spidertokill(2)),
                                 asserta(zooltokill(2)),
                                 asserta(wolftokill(3)),
                                 write('Your Quest is to kill 3 wolf , 2 spider and 2 zool'),nl,
+                                generateQuestReward(16,19),
                             );.
 
 generateQuestReward(Min,Max) :- random(Min,Max,QuestReward),
                             exp(X,curexp),
                             gold(X,curgold),
                             QuestExpReward = (QuestReward*100) + curexp,
-                            QuestGoldReward = (QuestReward*150) + curgold,
-                            retract(exp(X,_)), asserta(exp(X,QuestExpReward)),
-                            retract(gold(X,_)), asserta(gold(X,QuestGoldReward)).
+                            QuestGoldReward = (QuestReward*150) + curgold.
+
+/* Kalau Quest beres baru dapet ini, quest bisa beres dimana ajah*/
+updateExpAndGoldQuest :-
+                        retract(exp(X,_)), asserta(exp(X,QuestExpReward)),
+                        retract(gold(X,_)), asserta(gold(X,QuestGoldReward)).
 
 exitQuest :- write('Thank you adventurer, may the odds be ever in your favor'),nl.
+
+/* To Do
+- Cek apakah player ada di Q
+- Implement Progress dan finish Quest*/
